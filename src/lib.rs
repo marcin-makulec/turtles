2937e8ddc09cc6db4819cd59b004f8af07d8ce1f
marcin-makulec <m.makulec321@gmail.com>
2023-08-07 18:40:49 +0200

    Main finished, added empty fn get_critical_number

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+pub struct IndexedNumber {
+    pub index: u64,
+    pub value: u64,
+}
+
+pub fn get_critical_number(numbers: impl Iterator<Item = u64>) -> Option<IndexedNumber> {
+    None
+}
--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,25 @@ use std::env;
 use std::error;
 use std::fs;
 
+use turtles::get_critical_number;
+
 fn main() -> Result<(), Box<dyn error::Error>> {
     let args: Vec<String> = env::args().collect();
 
     let filename = args.get(1).ok_or("Usage: turtles <filename>")?;
     let input_text = fs::read_to_string(filename)?;
-    
-    let numbers: Vec<u64> = input_text
+
+    let numbers_iterator = input_text
         .lines()
-        .filter_map(|line| line.parse().ok())
-        .collect();
+        .filter_map(|line| line.parse::<u64>().ok());
+
+    match get_critical_number(numbers_iterator) {
+        Some(x) => println!(
+            "The tunnel will crumble at number {} on line {}",
+            x.value, x.index
+        ),
+        None => println!("The tunnel will not crumble"),
+    }
 
     Ok(())
 }
