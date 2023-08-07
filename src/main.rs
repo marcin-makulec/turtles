d3f42dce5245365012883636a16fa58b45dc220c
marcin-makulec <m.makulec321@gmail.com>
2023-08-07 18:32:09 +0200

    Handling user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::error;
+use std::fs;
+
+fn main() -> Result<(), Box<dyn error::Error>> {
+    let args: Vec<String> = env::args().collect();
+
+    let filename = args.get(1).ok_or("Usage: turtles <filename>")?;
+    let input_text = fs::read_to_string(filename)?;
+    
+    let numbers: Vec<u64> = input_text
+        .lines()
+        .filter_map(|line| line.parse().ok())
+        .collect();
+
+    Ok(())
 }
